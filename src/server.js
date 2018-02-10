0efdb65ce49233c5b424ca1712c3d159e6c6cb32
sretundijr <sretundijr@gmail.com>
2018-02-10 14:34:51 -0700

    create directory and download

--- src/server.js
+++ src/server.js
@@ -1,16 +1,34 @@
 const express = require('express');
 const { getIt } = require('hls-fetcher/index');
 const fs = require('fs');
-
-var cors = require('cors')
+const mkdirp = require('mkdirp');
+const path = require('path');
 
 const app = express();
 
 const PORT = 8080;
 
-app.get('/', (req, res) => {
+app.get('/stream', (req, res) => {
+  // create a stream subdir
+  const subDir = path.resolve(process.cwd(), 'streams');
 
+  console.log(req.query);
+  mkdirp(subDir, err => err ? console.error(err) : console.log('directory created'));
 
+  getIt(
+    {
+      uri: req.query.hlsurl,
+      cwd: `${process.cwd()}/streams`,
+    },
+    (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('files downloaded');
+        res.status(200).json({ hello: 'world' });
+      }
+    }
+  )
 
 });
 
--- src/test.js
+++ src/test.js
@@ -1,11 +1,18 @@
 
 const { getIt } = require('hls-fetcher/index');
+const path = require('path');
+const mkdirp = require('mkdirp');
 
+
+const subDir = path.resolve(process.cwd(), 'streams');
+
+mkdirp(subDir, (err) => err ? console.error(err) : console.log('success'));
 
 getIt(
   {
     uri: 'https://player.vimeo.com/external/249414131.m3u8?s=10bf9d088fff85588fdd56dacd5f9f716c1c8dd5',
     cwd: `${process.cwd()}/streams`,
   },
-  () => (console.log('done'))
+  (err) => err ? console.log(err) : console.log('downloaded successfully')
 )
+
